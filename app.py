238471032250494c8f7bb97566f560479d136bd7
benjaminyeom <benyeom042@gmail.com>
2025-01-22 13:52:42 -0800

    Stored csv into images folder.

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ df = pd.DataFrame({'age': 18,
 df.loc[1:, 'age'] = 16
 print(df)
 
-path = "/Users/benjaminyeom/Downloads/Copy of Intern Ranges 2024 - US Summary.csv"
+path = "images/Copy of Intern Ranges 2024 - US Summary.csv"
 df2 = pd.read_csv(path)
 print(df2)
